f45f51b9dff37ea9c3c470a074c0f786fe9ca9e9
Alexandr Momotuik <sasha.momotuik@gmail.com>
2017-11-23 23:50:57 +0200

    Final version WPF homework

---
+++ WpfApp_class_work_21112017/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp_class_work_21112017
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
